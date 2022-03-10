3932c53ba988c6648ccdb1d84e0e5631d7c3c144
VesiDaskalova <vessito@gmail.com>
2022-03-10 19:53:31 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number = 6;
+        String name = "Veselka";
+        String lastName = "Petkov";
+        System.out.println(lastName.length());
+
+        if (6 == number){
+            System.out.println("The number is 6");
+        }
+        if(lastName.equals("Petkov")) {
+            System.out.println("Last name is Petkov");
+        }
+    }
+}
